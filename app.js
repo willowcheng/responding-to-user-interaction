21888c3a42ddfc2c9bb7b5423f2198efdd1aa490
willowcheng <e.wingus@gmail.com>
2017-02-17 15:06:20 -0500

    The event object

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 const listItems = document.getElementsByTagName('li');
 
-listDiv.addEventListener('mouseover', () => {
-  listItems[i].textContent = listItems[i].textContent.toUpperCase();
+listDiv.addEventListener('mouseover', (event) => {
+  if (event.target.tagName == "LI") {
+    event.target.textContent = event.target.textContent.toUpperCase();
+  }
 });
 
-listDiv.addEventListener('mouseout', () => {
-  listItems[i].textContent = listItems[i].textContent.toLowerCase();
+listDiv.addEventListener('mouseout', (event) => {
+  if (event.target.tagName == "LI") {
+    event.target.textContent = event.target.textContent.toLowerCase();
+  }
 });
 
 toggleList.addEventListener('click', () => {
